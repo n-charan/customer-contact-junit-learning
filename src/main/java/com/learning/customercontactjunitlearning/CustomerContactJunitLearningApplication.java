adeac72fde9e61d2b121470434c802c3b83d3bff
Nishant <nishant.charan.21@gmail.com>
2022-09-07 12:20:17 +0530

    Code skeleton

---
+++ src/main/java/com/learning/customercontactjunitlearning/CustomerContactJunitLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.customercontactjunitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerContactJunitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerContactJunitLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/customercontactjunitlearning/CustomerContactJunitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.customercontactjunitlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerContactJunitLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
